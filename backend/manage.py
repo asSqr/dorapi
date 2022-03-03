af4e61b6931269b4672d3e672991934b87892365
Ysmr-Ry <ryo.yossy@live.jp>
2022-03-04 02:00:38 +0900

    fix: coverage

--- backend/manage.py
+++ backend/manage.py
@@ -2,9 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import dotenv
 
 
 def main():
+    dotenv.load_dotenv()
+
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings.local')
     try:
