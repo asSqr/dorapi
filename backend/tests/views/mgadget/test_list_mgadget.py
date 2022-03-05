ebc98fdc85a11fd00b79483968d4975e427ab7e8
Ysmr-Ry <ryo.yossy@live.jp>
2022-03-06 00:57:38 +0900

    test: list_mgadget keyword

--- backend/tests/views/mgadget/test_list_mgadget.py
+++ backend/tests/views/mgadget/test_list_mgadget.py
@@ -51,3 +51,52 @@ class TestListMGadget(TestCase):
             self.assertEqual(extras['count'], len(self.seeds.mgadgets))
 
         check_response()
+
+    def test_list_mgadgets_keyword(self):
+        """
+        キーワードによる検索
+        """
+        
+        keywords = ['めがね', 'くすり', 'ばくだん', 'らいと']
+        
+        for keyword in keywords:
+        
+            def keyword_filter_func(mgadget):
+                mbooks = mgadget.mbooks.all()
+                
+                return keyword in mgadget.name\
+                    or keyword in mgadget.ruby\
+                    or keyword in mgadget.desc\
+                    or keyword in map(lambda mbook: mbook.series, mbooks)\
+                    or keyword in map(lambda mbook: mbook.volume, mbooks)
+            
+            filtered_mgadgets = list(filter(keyword_filter_func, self.seeds.mgadgets))
+            
+            mgadget_dict = {}
+            
+            for mgadget in filtered_mgadgets:
+                mgadget_dict[mgadget.name] = mgadget
+            
+            url = f'{self.get_url()}?keyword={keyword}'
+
+            result_json = self.request.get(url).json()
+
+            def check_response():
+                datas = result_json['datas']
+                extras = result_json['extras']
+                
+                for data in datas:
+                    name = data['name']
+                    mgadget = mgadget_dict[name]
+                    
+                    self.assertEqual(data['name'], mgadget.name)
+                    self.assertEqual(data['ruby'], mgadget.ruby)
+                    self.assertEqual(data['desc'], mgadget.desc)
+                    
+                    for book, mbook in zip(data['mbooks'], mgadget.mbooks.all()):
+                        self.assertEqual(BookSeriesEnum[book['series']].value, mbook.series)
+                        self.assertEqual(book['volume'], mbook.volume)
+                    
+                self.assertEqual(extras['count'], len(filtered_mgadgets))
+
+            check_response()
