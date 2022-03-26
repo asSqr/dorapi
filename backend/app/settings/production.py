736dc6797830f53799857e22c7d9a411e44abb2a
Ysmr-Ry <ryo.yossy@live.jp>
2022-03-26 11:16:25 +0900

    fix: CORS

--- backend/app/settings/production.py
+++ backend/app/settings/production.py
@@ -11,7 +11,10 @@ ALLOWED_HOSTS = ['*']  # 環境によっては変更可能性アリ
 
 # CORS設定
 # TODO: 本番ビルド時はlocalhost等を無効化する
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'https://earth-bird.vercel.app',
+]
 
 CORS_ALLOW_METHODS = [
     'DELETE',
