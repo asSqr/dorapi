37a7f53cb26bc02eb99750a21d8bfba1ee638e8c
Ysmr-Ry <ryo.yossy@live.jp>
2022-04-06 09:42:46 +0900

    fix: custom_search asyncio (TODO: async_request)

---
+++ backend/shells/custom_search/custom_search.py
@@ -0,0 +1,93 @@
+from constants import (
+    GOOGLE_BASE_URL,
+    GOOGLE_CUSTOM_SEARCH_PATH,
+    GOOGLE_CUSTOM_DORA_SUFFIX,
+    WAIT_SECONDS,
+)
+from utils import generate_query
+
+import dotenv
+import time
+import os
+import sys
+import requests
+from urllib.parse import quote
+from dataclasses import dataclass
+import asyncio
+from typing import List
+
+sys.path.append('./backend/seeds/gadgets')
+
+from data_struct import Gadget
+
+
+dotenv.load_dotenv()
+
+
+@dataclass
+class SearchInfo:
+    image_url: str
+    total_results: int
+
+
+async def get_image_url_from_google(gadget_name: str) -> SearchInfo:
+    
+    CSE_ID = os.getenv('CSE_ID')
+    GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+    
+    search_keyword = f'{gadget_name} コミック'
+    encoded_keyword = quote(search_keyword.replace(' ', '+'))
+    
+    query_dict = {
+        'q': encoded_keyword,
+        'searchType': 'image',
+        'cx': CSE_ID,
+        'key': GOOGLE_API_KEY,
+        'start': 1,
+        'lr': 'lang_ja',
+        'num': 1,
+    }
+    
+    query = generate_query(query_dict)
+    
+    url = f'{GOOGLE_BASE_URL}{GOOGLE_CUSTOM_SEARCH_PATH}?{query}'
+    
+    resp = requests.get(url).json()
+    images = resp.get('items', [{'link': None}])
+    infos = resp.get('searchInformation', {})
+    
+    image_url = images[0]['link']
+    total_results = infos.get('totalResults')
+    
+    return SearchInfo(
+        image_url=image_url,
+        total_results=total_results,
+    )
+
+
+async def process(gadget: Gadget) -> None:
+    search_keyword = f'{gadget.name}{GOOGLE_CUSTOM_DORA_SUFFIX}'
+    search_info = await get_image_url_from_google(search_keyword)
+    gadget.image_url = search_info.image_url
+    
+    print(f'#{search_info.total_results}: {search_info.image_url}', file=sys.stderr)
+    
+    if gadget.image_url is None:
+        time.sleep(WAIT_SECONDS)
+        await process(gadget)
+        return
+    
+    gadget.total_results = search_info.total_results
+    
+    if gadget.total_results is None:
+        time.sleep(WAIT_SECONDS)
+        await process(gadget)
+        return
+
+
+def worker(gadgets: List[Gadget]) -> None:
+    loop = asyncio.get_event_loop()
+    gather = asyncio.gather(
+        *[process(gadget) for gadget in gadgets]
+    )
+    loop.run_until_complete(gather)
--- backend/shells/gadgets/dora_superdatabase.py
+++ backend/shells/gadgets/dora_superdatabase.py
@@ -1,11 +1,3 @@
-from dataclasses import dataclass
-
-import dotenv
-
-import os
-import requests
-from urllib.parse import quote
-
 import time
 import sys
 
@@ -36,13 +28,8 @@ from typing import List, Tuple      # noqa
 
 sys.path.append('backend/seeds/gadgets')
 
-from custom_search.constants import (
-    GOOGLE_BASE_URL,
-    GOOGLE_CUSTOM_SEARCH_PATH,
-    GOOGLE_CUSTOM_DORA_SUFFIX,
-    WAIT_SECONDS,
-)
-from custom_search.utils import generate_query
+from custom_search import worker
+
 
 GADGET_ARTICLE_PATH = 'article'
 
@@ -331,71 +318,6 @@ def get_gadgets() -> Tuple[List[Gadget], List[Link]]:
 
 
 (gadgets, links) = get_gadgets()
-
-
-dotenv.load_dotenv()
-
-
-@dataclass
-class SearchInfo:
-    image_url: str
-    total_results: int
-
-
-def get_image_url_from_google(gadget_name: str) -> SearchInfo:
-    
-    CSE_ID = os.getenv('CSE_ID')
-    GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
-    
-    search_keyword = f'{gadget_name} コミック'
-    encoded_keyword = quote(search_keyword.replace(' ', '+'))
-    
-    query_dict = {
-        'q': encoded_keyword,
-        'searchType': 'image',
-        'cx': CSE_ID,
-        'key': GOOGLE_API_KEY,
-        'start': 1,
-        'lr': 'lang_ja',
-        'num': 1,
-    }
-    
-    query = generate_query(query_dict)
-    
-    url = f'{GOOGLE_BASE_URL}{GOOGLE_CUSTOM_SEARCH_PATH}?{query}'
-    
-    resp = requests.get(url).json()
-    images = resp.get('items', [{'link': None}])
-    infos = resp.get('searchInformation', {})
-    
-    image_url = images[0]['link']
-    total_results = infos.get('totalResults')
-    
-    return SearchInfo(
-        image_url=image_url,
-        total_results=total_results,
-    )
-
-
-def crawler() -> None:
-    for gadget in gadgets:
-        search_keyword = f'{gadget.name}{GOOGLE_CUSTOM_DORA_SUFFIX}'
-        search_info = get_image_url_from_google(search_keyword)
-        gadget.image_url = search_info.image_url
-        
-        print(f'#{search_info.total_results}: {search_info.image_url}', file=sys.stderr)
-        
-        if gadget.image_url is None:
-            time.sleep(WAIT_SECONDS)
-            continue
-        
-        gadget.total_results = search_info.total_results
-        
-        if gadget.total_results is None:
-            time.sleep(WAIT_SECONDS)
-            continue
-        
-        time.sleep(WAIT_SECONDS)
 
 
 crawler()
